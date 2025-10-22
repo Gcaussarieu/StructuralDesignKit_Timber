588bcb24111a3755919a0f2efb660766bd23afcd
Guillaume Caussarieu <guillaume.caussarieu@gmail.com>
2025-10-22 11:21:31 +0200

    Updated vibration calculation engine for Transient floor based on the prEN 1995-1-1

--- StructuralDesignKitExcel/ExcelFormulaeVibrations.cs
+++ StructuralDesignKitExcel/ExcelFormulaeVibrations.cs
@@ -21,6 +21,7 @@ namespace StructuralDesignKitExcel
 
 		/// <summary>
 		/// Compute the Resonant Response Analysis for Low frequency floors for a given point with multiple vibration modes (RMS acceleration of the response)
+		/// The input is formated for RFEM 6
 		/// </summary>
 		/// <param name="Modes">Range of modes considered</param>
 		/// <param name="Frequencies">Range of frequencies considered</param>
@@ -94,6 +95,203 @@ namespace StructuralDesignKitExcel
 
 			return accelerationResponse;
 		}
+
+
+		/// <summary>
+		/// Compute the Transient Response Analysis for high frequency floors for a given point with multiple vibration modes (RMS velocity of the response)
+		/// The input is formated for RFEM 6
+		/// </summary>
+		/// <param name="Modes">Range of modes considered</param>
+		/// <param name="Frequencies">Range of frequencies considered</param>
+		/// <param name="ModalMasses">Range of modal masses considered</param>
+		/// <param name="StandardizedDisplacements">Data for the standardized displacement according to the documentation</param>
+		/// <param name="fp">Pace frequency in [Hz]</param>
+		/// <param name="Xi">Damping as ratio to critical damping</param>
+		/// <param name="SurfaceIDExcitation">Surface on which the node for the excitation to consider is situated</param>
+		/// <param name="ExcitationNode">Node to consider for the excitation</param>
+		/// <param name="SurfaceIDResponse">Surface on which the node for the response to consider is situated</param>
+		/// <param name="ResponseNode">Node to consider for the excitation</param>
+		/// <param name="ResponseFactor">If true, provide the Response factor instead of the acceleration</param>
+		/// <returns></returns>
+		[ExcelFunction(Description = "Provide the velocity response for a given mode and timestep",
+			Name = "SDK.Vibration.VelocitySeries",
+			IsHidden = false,
+			Category = "SDK.Vibration")]
+		public static object VelocityResponseVelocitySeries(
+			[ExcelArgument(Description = "Range of modes considered")] object[,] Modes,
+			[ExcelArgument(Description = "Range of frequencies considered")] object[,] Frequencies,
+			[ExcelArgument(Description = "Range of modal masses considered")] object[,] ModalMasses,
+			[ExcelArgument(Description = "Data for the standardized displacement according to the documentation")] object[,] StandardizedDisplacements,
+			[ExcelArgument(Description = "Pace frequency in [Hz]")] double fp,
+			[ExcelArgument(Description = "Damping as ratio to critical damping")] double Xi,
+			[ExcelArgument(Description = "Surface on which the node for the excitation to consider is situated")] int SurfaceIDExcitation,
+			[ExcelArgument(Description = "Node to consider for the excitation")] int ExcitationNode,
+			[ExcelArgument(Description = "Surface on which the node for the response to consider is situated")] int SurfaceIDResponse,
+			[ExcelArgument(Description = "Node to consider for the response")] int ResponseNode,
+			[ExcelArgument(Description = "If true, provide the Response factor instead of the acceleration")] bool ResponseFactor,
+			[ExcelArgument(Description = "Natural mode to consider")] int mode,
+			[ExcelArgument(Description = "time step to consider")] int step,
+			[ExcelArgument(Description = "the resolution in second of the analysis - default value is 0.00125s")] double timeStep = 0.00125d)
+		{
+			double VelocityResponse = 0;
+			try
+			{
+				if (step > (1.0 / fp) / timeStep) return ExcelEmpty.Value;
+				else
+				{
+
+
+					//ensure all input lists have the same length
+					int listLength = Modes.Length;
+					if (Frequencies.Length != listLength || ModalMasses.Length != listLength) throw new ArgumentException("all list should have the same length");
+
+					//range to lists
+					List<int> modes = new List<int>();
+					List<double> frequencies = new List<double>();
+					List<double> modalMasses = new List<double>();
+
+
+					for (int i = 0; i < Modes.Length; i++)
+					{
+						modes.Add(Convert.ToInt32(Modes[i, 0]));
+						frequencies.Add(Convert.ToDouble(Frequencies[i, 0]));
+						modalMasses.Add(Convert.ToDouble(ModalMasses[i, 0]));
+					}
+
+					var data = FormatExcelRFEM6Data(modes, StandardizedDisplacements);
+
+					//Get the two nodes selected for the analysis
+					VibrationResultNode excitationResultNode = data.Where(p => p.SurfaceId == SurfaceIDExcitation).Where(p => p.PointID == ExcitationNode).First();
+					VibrationResultNode responseResultNode = data.Where(p => p.SurfaceId == SurfaceIDResponse).Where(p => p.PointID == ResponseNode).First();
+
+
+					List<double> velocitySeries = new List<double>();
+
+
+					if (mode >= 1 && mode <= frequencies.Count)
+					{
+						double Imod_ef = Vibrations.ComputeEffectiveFootfallImpulseEC5(fp, frequencies[mode - 1]);
+						return Vibrations.VelocityResponse(
+										excitationResultNode.NormalisedDisplacements.Values.ElementAt(0)[2][mode - 1],
+										responseResultNode.NormalisedDisplacements.Values.ElementAt(0)[2][mode - 1],
+										frequencies[mode - 1],
+										fp,
+										Imod_ef,
+										Xi,
+										step * timeStep,
+										modalMasses[mode - 1]
+										);
+
+					}
+
+					else if (mode == -1)
+					{
+						double v = 0;
+						for (int i = 0; i < frequencies.Count; i++)
+						{
+							double Imod_ef = Vibrations.ComputeEffectiveFootfallImpulseEC5(fp, frequencies[i]);
+
+							v += Vibrations.VelocityResponse(
+									excitationResultNode.NormalisedDisplacements.Values.ElementAt(0)[2][i],
+									responseResultNode.NormalisedDisplacements.Values.ElementAt(0)[2][i],
+									frequencies[i],
+									fp,
+									Imod_ef,
+									Xi,
+									step * timeStep,
+									modalMasses[i]
+									);
+						}
+						return v;
+					}
+
+					else throw new Exception("The mode provided is not coherent");
+				}
+			}
+			catch (Exception e)
+			{
+
+				MessageBox.Show(e.Message);
+			}
+
+			return -1;
+		}
+
+		/// <summary>
+		/// Compute the Transient Response Analysis for high frequency floors for a given point with multiple vibration modes (RMS velocity of the response)
+		/// The input is formated for RFEM 6
+		/// </summary>
+		/// <param name="Modes">Range of modes considered</param>
+		/// <param name="Frequencies">Range of frequencies considered</param>
+		/// <param name="ModalMasses">Range of modal masses considered</param>
+		/// <param name="StandardizedDisplacements">Data for the standardized displacement according to the documentation</param>
+		/// <param name="fp">Pace frequency in [Hz]</param>
+		/// <param name="Xi">Damping as ratio to critical damping</param>
+		/// <param name="SurfaceIDExcitation">Surface on which the node for the excitation to consider is situated</param>
+		/// <param name="ExcitationNode">Node to consider for the excitation</param>
+		/// <param name="SurfaceIDResponse">Surface on which the node for the response to consider is situated</param>
+		/// <param name="ResponseNode">Node to consider for the excitation</param>
+		/// <param name="ResponseFactor">If true, provide the Response factor instead of the acceleration</param>
+		/// <param name="timeStep">the resolution in second of the analysis - default value is 0.00125s</param>
+		/// <returns></returns>
+		[ExcelFunction(Description = "Compute the Transient Response Analysis for high frequency floors for a given point with multiple vibration modes (RMS velocity of the response)",
+			Name = "SDK.Vibration.VelocityResponse",
+			IsHidden = false,
+			Category = "SDK.Vibration")]
+		public static double VelocityResponse(
+			[ExcelArgument(Description = "Range of modes considered")] object[,] Modes,
+			[ExcelArgument(Description = "Range of frequencies considered")] object[,] Frequencies,
+			[ExcelArgument(Description = "Range of modal masses considered")] object[,] ModalMasses,
+			[ExcelArgument(Description = "Data for the standardized displacement according to the documentation")] object[,] StandardizedDisplacements,
+			[ExcelArgument(Description = "Pace frequency in [Hz]")] double fp,
+			[ExcelArgument(Description = "Damping as ratio to critical damping")] double Xi,
+			[ExcelArgument(Description = "Surface on which the node for the excitation to consider is situated")] int SurfaceIDExcitation,
+			[ExcelArgument(Description = "Node to consider for the excitation")] int ExcitationNode,
+			[ExcelArgument(Description = "Surface on which the node for the response to consider is situated")] int SurfaceIDResponse,
+			[ExcelArgument(Description = "Node to consider for the response")] int ResponseNode,
+			[ExcelArgument(Description = "If true, provide the Response factor instead of the acceleration")] bool ResponseFactor,
+			[ExcelArgument(Description = "the resolution in second of the analysis - default value is 0.00125s")] double timeStep = 0.00125d)
+		{
+			double VelocityResponse = 0;
+			try
+			{
+				//ensure all input lists have the same length
+				int listLength = Modes.Length;
+				if (Frequencies.Length != listLength || ModalMasses.Length != listLength) throw new ArgumentException("all list should have the same length");
+
+				//range to lists
+				List<int> modes = new List<int>();
+				List<double> frequencies = new List<double>();
+				List<double> modalMasses = new List<double>();
+
+
+				for (int i = 0; i < Modes.Length; i++)
+				{
+					modes.Add(Convert.ToInt32(Modes[i, 0]));
+					frequencies.Add(Convert.ToDouble(Frequencies[i, 0]));
+					modalMasses.Add(Convert.ToDouble(ModalMasses[i, 0]));
+				}
+
+				var data = FormatExcelRFEM6Data(modes, StandardizedDisplacements);
+
+				//Get the two nodes selected for the analysis
+				VibrationResultNode excitationResultNode = data.Where(p => p.SurfaceId == SurfaceIDExcitation).Where(p => p.PointID == ExcitationNode).First();
+				VibrationResultNode responseResultNode = data.Where(p => p.SurfaceId == SurfaceIDResponse).Where(p => p.PointID == ResponseNode).First();
+
+				VelocityResponse = Vibrations.TransientResponseAnalysis(
+					excitationResultNode.NormalisedDisplacements.Values.ElementAt(0)[2],
+					responseResultNode.NormalisedDisplacements.Values.ElementAt(0)[2],
+					frequencies, modalMasses, fp, Xi, ResponseFactor, timeStep);
+			}
+			catch (Exception e)
+			{
+
+				MessageBox.Show(e.Message);
+			}
+
+			return VelocityResponse;
+		}
+
 
 		/// <summary>
 		/// Compute the Resonant Response Analysis for Low frequency floors for a given point with multiple vibration modes (RMS acceleration of the response)
@@ -281,30 +479,44 @@ namespace StructuralDesignKitExcel
 
 
 			//Extract data from the Excel selected range and populate the list of VibrationResultPoint 
-			for (int i = 0; i < rowLength ; i += modes.Count)
+			for (int i = 0; i < rowLength; i += modes.Count)
 			{
 				Dictionary<List<int>, List<List<double>>> inputData = new Dictionary<List<int>, List<List<double>>>();
 				List<double> ux = new List<double>();
 				List<double> uy = new List<double>();
 				List<double> uz = new List<double>();
 
-				for (int j = 0; j < modes.Count; j++)
+
+				//It happens that RFEM6 specifies the message "The result value in the point is not defined." when a value on surface is invalid
+				//This is the case for non rectangular surfases 
+				//In such case, the point is ignored
+				if (Convert.ToString(standardizedDisplacementsInput[i, 6]) != "The result value in the point is not defined.")
 				{
-					ux.Add(Convert.ToDouble(standardizedDisplacementsInput[i+j, 6]));
-					uy.Add(Convert.ToDouble(standardizedDisplacementsInput[i+j, 7]));
-					uz.Add(Convert.ToDouble(standardizedDisplacementsInput[i+j, 8]));
+					for (int j = 0; j < modes.Count; j++)
+					{
+						ux.Add(Convert.ToDouble(standardizedDisplacementsInput[i + j, 6]));
+						uy.Add(Convert.ToDouble(standardizedDisplacementsInput[i + j, 7]));
+						uz.Add(Convert.ToDouble(standardizedDisplacementsInput[i + j, 8]));
+					}
+
+					inputData.Add(modes, new List<List<double>> { ux, uy, uz });
+
+					int surfaceId = Convert.ToInt32(standardizedDisplacementsInput[i, 0]);
+					int pointID = Convert.ToInt32(standardizedDisplacementsInput[i, 1]);
+					double x = Convert.ToDouble(standardizedDisplacementsInput[i, 2]);
+					double y = Convert.ToDouble(standardizedDisplacementsInput[i, 3]);
+					double z = Convert.ToDouble(standardizedDisplacementsInput[i, 4]);
+
+					output.Add(new VibrationResultNode(pointID, surfaceId, x, y, z, inputData));
 				}
-
-				inputData.Add(modes, new List<List<double>> { ux, uy, uz });
-
-				int surfaceId = Convert.ToInt32(standardizedDisplacementsInput[i, 0]);
-				int pointID = Convert.ToInt32(standardizedDisplacementsInput[i, 1]);
-				double x = Convert.ToDouble(standardizedDisplacementsInput[i, 2]);
-				double y = Convert.ToDouble(standardizedDisplacementsInput[i, 3]);
-				double z = Convert.ToDouble(standardizedDisplacementsInput[i, 4]);
-
-
-				output.Add(new VibrationResultNode(pointID, surfaceId, x, y, z, inputData));
+				else
+				{
+					i += modes.Count;
+					if (blankRows.Contains(i))
+					{
+						i++;
+					}
+				}
 
 
 				if (blankRows.Contains(i + modes.Count))
